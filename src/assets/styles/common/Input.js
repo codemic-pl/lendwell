2797af83cb540e6377e3a816f5d6aaaa20f8a0d8
Jan Schwichtenberg <janschwichtenberg@gmail.com>
2019-07-12 00:31:38 +0200

    Add input component

---
+++ src/assets/styles/common/Input.js
@@ -0,0 +1,46 @@
+import { StyleSheet } from 'react-native';
+import {
+  GRAY_COLOR,
+  TEXT_COLOR
+} from './Variables';
+
+export default StyleSheet.create({
+  label: {
+    fontFamily: 'oxygen',
+    fontWeight: 'normal',
+    fontSize: 11,
+    letterSpacing: 2,
+    color: GRAY_COLOR,
+    textTransform: 'uppercase'
+  },
+  input: {
+    color: TEXT_COLOR,
+    flex: 1,
+    fontSize: 15,
+    lineHeight: 20,
+    fontFamily: 'oxygen',
+    fontWeight: 'bold',
+    paddingTop: 16,
+    paddingBottom: 16,
+    paddingLeft: 24,
+    paddingRight: 24
+  },
+  inputContainer: {
+    textAlignVertical: 'center',
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: GRAY_COLOR
+  },
+  disabled: {
+
+  },
+  container: {
+    marginVertical: 4,
+  },
+  containerInline: {
+    flex: 1
+  },
+  containerWithLabel: {
+    paddingTop: 8
+  }
+});
---
+++ src/components/common/Input.js
@@ -0,0 +1,133 @@
+import React from 'react';
+import { TextInput, View, Text } from 'react-native';
+import componentStyles from '../../assets/styles/common/Input';
+import { GRAY_COLOR } from '../../assets/styles/common/Variables';
+
+const Input = ({
+  label,
+  value,
+  onChangeText,
+  placeholder,
+  secureTextEntry,
+  disabled,
+  isRow,
+  keyboardType,
+  hide,
+  onBlur,
+  onFocus,
+  onEndEditing,
+  autoCapitalize,
+  numberOfLines,
+  labelStyle,
+  inputStyle,
+  containerStyle,
+  inputContainerStyle
+}) => {
+  if (hide) {
+    return null;
+  }
+
+  const renderLabel = () => {
+    if (label) {
+      return <Text style={[componentStyles.label, labelStyle]}>{label}</Text>;
+    }
+    return null;
+  };
+
+  const renderFormControl = () => {
+    return (
+      <View
+        style={[
+          componentStyles.inputContainer,
+          inputContainerStyle
+        ]}
+      >
+        {renderInput()}
+      </View>
+    );
+  };
+
+  const renderInput = () => {
+    if (disabled) {
+      return (
+        <Text
+          style={[
+            componentStyles.input,
+            componentStyles.disabled,
+            inputStyle,
+            {
+              minHeight: (numberOfLines && numberOfLines > 1 ? numberOfLines * 20 : 0)
+            }
+          ]}
+          multiline={(numberOfLines && numberOfLines > 1)}
+          numberOfLines={numberOfLines || 1}
+        >
+          {value}
+        </Text>
+      );
+    }
+
+    if (keyboardType) {
+      return (<TextInput
+        secureTextEntry={secureTextEntry}
+        placeholder={placeholder}
+        autoCorrect={false}
+        style={[
+          componentStyles.input,
+          inputStyle,
+          {
+            minHeight: (numberOfLines && numberOfLines > 1 ? numberOfLines * 20 : 0)
+          }
+        ]}
+        placeholderTextColor={GRAY_COLOR}
+        multiline={(numberOfLines && numberOfLines > 1)}
+        numberOfLines={numberOfLines || 1}
+        value={value}
+        keyboardType={keyboardType}
+        onChangeText={onChangeText}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        onEndEditing={onEndEditing}
+        autoCapitalize={autoCapitalize}
+      />);
+    }
+    return (<TextInput
+      secureTextEntry={secureTextEntry}
+      placeholder={placeholder}
+      autoCorrect={false}
+      style={[
+        componentStyles.input,
+        inputStyle,
+        {
+          minHeight: (numberOfLines && numberOfLines > 1 ? numberOfLines * 20 : 0)
+        }
+      ]}
+      placeholderTextColor={GRAY_COLOR}
+      multiline={(numberOfLines && numberOfLines > 1)}
+      numberOfLines={numberOfLines || 1}
+      value={value}
+      onChangeText={onChangeText}
+      onFocus={onFocus}
+      onBlur={onBlur}
+      onEndEditing={onEndEditing}
+      autoCapitalize={autoCapitalize}
+    />);
+  };
+
+  return (
+    <View
+      style={[
+        componentStyles.container,
+        (isRow ? componentStyles.containerInline : null),
+        (label ? componentStyles.containerWithLabel : null),
+        containerStyle
+      ]}
+    >
+      {renderLabel()}
+      {renderFormControl()}
+    </View>
+  );
+};
+
+
+export { Input };
