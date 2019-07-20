1f4c885a5523fa362bb077998318083723499e92
Jan Schwichtenberg <janschwichtenberg@gmail.com>
2019-07-20 02:18:15 +0200

    Dodanie obsługi alertów

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import DropdownAlert from 'react-native-dropdownalert';
+import { AlertHelper } from './helpers/AlertHelper';
 import Router from './configs/Router';
 import configStore from './configs/store';
 
@@ -12,6 +14,7 @@ class App extends Component {
         <PersistGate loading={null} persistor={persistor}>
           <Router />
         </PersistGate>
+        <DropdownAlert ref={ref => AlertHelper.setDropDown(ref)} closeInterval={6000} />
       </Provider>
     );
   }
