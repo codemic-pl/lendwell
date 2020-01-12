159c7fd4056e6008e54a3f494f389fce5e1fdf2a
Jan Schwichtenberg <janschwichtenberg@gmail.com>
2020-01-12 05:09:42 +0100

    use moment to format dates

--- src/utils/mixins.js
+++ src/utils/mixins.js
@@ -1,25 +1,11 @@
+import moment from 'moment';
+
 const getDateYMD = (someDate) => {
-  const date = someDate || new Date();
-  const Y = new Date(date).getFullYear();
-  let M = new Date(date).getMonth() + 1;
-  if (M < 10) {
-    M = `0${M}`;
-  }
-  const D = new Date(date).getDate();
-  return `${Y}-${M}-${D}`;
+  return moment(someDate).format('YYYY-MM-DD');
 };
 
 const formatDate = (someDate) => {
-  const date = someDate || new Date();
-  const Y = new Date(date).getFullYear();
-  let M = new Date(date).getMonth() + 1;
-  if (M < 10) {
-    M = `0${M}`;
-  }
-  const D = new Date(date).getDate();
-  return `${D}.${M}.${Y}`;
-  // const date = someDate || new Date();
-  // return new Date(date).toLocaleDateString('pl-PL');
+  return moment(someDate).format('DD.MM.YYYY');
 };
 
 export { getDateYMD, formatDate };
