6eb350365a44d876af54a7a0957b28045edf5c5e
Jan Schwichtenberg <janschwichtenberg@gmail.com>
2020-01-12 17:55:08 +0100

    configure react-native-firebase at ios

---
+++ ios/Pods/Target Support Files/Pods-lendwell-tvOS/Pods-lendwell-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_lendwell_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_lendwell_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-lendwellTests/Pods-lendwellTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_lendwellTests : NSObject
+@end
+@implementation PodsDummy_Pods_lendwellTests
+@end
